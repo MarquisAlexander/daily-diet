40108292f824e81579e423bfca93baef114641fa
marquisalexander <marquis.alexander@outlook.com>
2023-01-05 19:43:28 -0300

    feat: added theme

--- App.tsx
+++ App.tsx
@@ -1,17 +1,19 @@
 import "react-native-gesture-handler";
 import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components";
 
 import { Routes } from "./src/routes/index.routes";
+import theme from "./src/theme";
 
 export default function App() {
 	return (
-		<>
+		<ThemeProvider theme={theme}>
 			<StatusBar
 				barStyle="dark-content"
 				translucent
 				backgroundColor="transparent"
 			/>
 			<Routes />
-		</>
+		</ThemeProvider>
 	);
 }
---
+++ src/theme/index.ts
@@ -0,0 +1,19 @@
+export default {
+    COLORS: {
+        WHITE: '#FFFFFF',
+
+        GRAY_200: '#333638',
+        GRAY_100: '#1B1D1E',
+
+        GREEN_DARK: '#639339',
+        GREEN_MID: '#CBE4B4',
+        GREEN_LIGHT: '#E5F0DB',
+    },
+    FONT_SIZE: {
+        XXS: 12,
+        XS: 14,
+        S: 18,
+        M: 24,
+        G: 32
+    }
+}
